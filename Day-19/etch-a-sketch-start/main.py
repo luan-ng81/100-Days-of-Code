0d6a332832c3b9bebade973e12f3b8a45247e1d5
luan-ng81 <luannguyen8118@gmail.com>
2025-08-28 23:59:27 -0700

    Completed Snake Game

---
+++ Day-19/etch-a-sketch-start/main.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tom = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+def move_backwards():
+    tim.backward(10)
+def move_clockwise():
+    tim.right(10)
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key= "d", fun= move_clockwise)
+screen.exitonclick()
